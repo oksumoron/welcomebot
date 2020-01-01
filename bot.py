e4c9850c5ed3fa7e91ce37c936773482abf7d4d0
Kravchuk Kseniya <k.kravchuk@corp.mail.ru>
2020-01-01 17:10:22 +0300

    remove all stuff

--- bot.py
+++ bot.py
@@ -377,70 +377,6 @@ def empty_message(bot, update):
             return goodbye(bot, update)
 
 
-USERS_AND_TIMEZONES = [{"id": 205459208, "name": "Kseniya", "tz": "Europe/Moscow", "congrats": False}, #+3
-                       #{"id": 818120570, "name": "Maybe", "tz":"Europe/Bucharest"}, #+2
-                       {"id": 217474162, "name": "Stazyros", "tz": "Europe/Moscow", "congrats": False}, # +3
-                       {"id": 750432187, "name": "Shakhnaz", "tz": "Asia/Krasnoyarsk", "congrats": False}, #+7
-                       {"id": 850842867, "name": "Olga", "tz": "Europe/Moscow", "congrats": False}, #+3
-                       {"id": 984037790, "name": "Julia", "tz": "Europe/Berlin", "congrats": False}, #+1
-                       {"id": 909049413, "name": "Angelika", "tz": "Europe/Warsaw", "congrats": False}, # +1
-                        {"id": 933314516, "name": "Nika", "tz": "Europe/Warsaw", "congrats": False}, # +1
-                        {"id": 1052078964, "name": "ess", "tz": "Asia/Dubai", "congrats": False}, # +4
-                        {"id": 60815732, "name": "Söph", "tz": "Europe/Berlin", "congrats": False}, #+1
-                        {"id": 819800093, "name": "Lea", "tz": "Europe/Berlin", "congrats": False}, #+1
-                        {"id": 903096807, "name": "nadine", "tz": "Europe/Berlin", "congrats": False}, #+1
-                        {"id": 906207913, "name": "Flora", "tz": "Europe/Berlin", "congrats": False}, #+1
-                        {"id": 818329880, "name": "Michi", "tz": "Europe/Berlin", "congrats": False}, #+1
-                        {"id": 312356585, "name": "Lily", "tz": "Europe/Berlin", "congrats": False}, #+1
-                        {"id": 1032883707, "name": "Haven", "tz": "America/Detroit", "congrats": False}, #-5
-                        {"id": 1065439413, "name": "t.b.p.f (Este)", "tz": "Europe/Rome", "congrats": False}, #+1
-                        {"id": 918544312, "name": "Allie", "tz": "Pacific/Auckland", "congrats": False}, #+13
-                        {"id": 865543531, "name": "Priscilla", "tz": "America/Manaus", "congrats": False}, #-4
-                        {"id": 999159684, "name": "Steph", "tz": "America/Detroit", "congrats": False}, #+1
-                        {"id": 843754913, "name": "Harper", "tz": "America/Menominee", "congrats": False}, #-6
-                        {"id": 234021809, "name": "A. B.", "tz": "Europe/Madrid", "congrats": False}, #+1
-                        {"id": 986930541, "name": "Lou", "tz": "Europe/Zurich", "congrats": False}, #+1
-                        {"id": 443578761, "name": "Sveta", "tz": "Europe/Moscow", "congrats": False}, #+2
-                        {"id": 938002879, "name": "blue", "tz": "Europe/Ljubljana", "congrats": False}, #+1
-                        {"id": 877331016, "name": "Marijke", "tz": "Europe/Berlin", "congrats": False}, #+1
-                        {"id": 841877693, "name": "Sarah", "tz": "Australia/Melbourne", "congrats": False}, #+11
-                        {"id": 843009397, "name": "Franzi", "tz": "Europe/Berlin", "congrats": False}, #+1
-                        {"id": 912600437, "name": "taru", "tz": "Europe/Helsinki", "congrats": False}, #+2
-                        {"id": 991666098, "name": "Philline", "tz": "Europe/Berlin", "congrats": False}, #+1
-                        {"id": 972172724, "name": "Paula", "tz": "Europe/Berlin", "congrats": False}, #+1
-                        {"id": 52136768, "name": "Ale", "tz": "Europe/Rome", "congrats": False}, #+1
-                        {"id": 1003023736, "name": "luna", "tz": "Europe/Rome", "congrats": False}, #+1
-                        {"id": 950621247, "name": "cassie", "tz": "America/Los_Angeles", "congrats": False}, #-8
-                        {"id": 901885283, "name": "sera", "tz": "America/New_York", "congrats": False}, #+1
-                        {"id": 967188191, "name": "teresa", "tz": "Europe/Berlin", "congrats": False}, #+1
-                        {"id": 712021173, "name": "Asya", "tz": "Europe/Moscow", "congrats": False}, #+3
-                        {"id": 103238867, "name": "Ira", "tz": "Europe/Moscow", "congrats": False}, #+3
-                        {"id": 885612343, "name": "Annabell", "tz": "Europe/Berlin", "congrats": False}, #+1
-                        {"id": 296825310, "name": "Nina", "tz": "Europe/Berlin", "congrats": False}, #+1
-                        {"id": 491155381, "name": "Helle", "tz": "Europe/Berlin", "congrats": False}, #+1
-                        {"id": 781889525, "name": "A", "tz": "America/Los_Angeles", "congrats": False}, #-8
-                        {"id": 557806090, "name": "Asher", "tz": "Europe/London", "congrats": False}, # +0
-
-
-]
-
-congrats_mgs = [{"msg": "My dear {}, 2020 has already come in your timezone! Yaaay! :partying_face: May this year bring new inspirations and a lot of happiness! Happy New Year!", "sent": False},
-                {"msg": "{}, 2020 is coming to your timezone and I hope it will be the best year in your life! Happy New Year! :partying_face:", "sent": False},
-                {"msg": "My lovely {} :red_heart:! Wishing you a Happy New Year with the hope that you will have many blessings in the year to come.", "sent": False},
-                {"msg": "{}, Happy New Year! :green_heart: May this year bring new happiness, new goals, new achievements and a lot of new inspirations on your life. Wishing you a year fully loaded with happiness.", "sent": False},
-                {"msg": "Dear {}! May the new year bring you warmth, love and light to guide your path to a positive destination. Here’s wishing you all the joy of the season. Have a Happy New Year! :partying_face:", "sent": False},
-                {"msg": "{}! Wishing you a year that’s promising, exciting, inspiring and full of fun! Happy New Year! :yellow_heart:", "sent": False},
-                {"msg": "{}! May your 2020 arrive with hope and a bag full of blessings. Have a prosperous and healthy New Year! :red_heart:", "sent": False},
-                {"msg": "My darling {} :blue_heart:! Wishing you a year that’s promising, exciting, inspiring and full of fun! Happy New Year!", "sent": False},
-                {"msg": "{}! It's 2020 in your house! Wishing you a new year rich with lost of love, joy, warmth, and laughter.", "sent": False},
-                {"msg": "My lovely {}! :orange_heart: Cheers to a new year and a fond farewell to the old. May you have a prosperous and healthy New Year!", "sent": False},
-                {"msg": "May 2020 bring you much joy and fun :partying_face: May you find peace, love and success. Sending my heartiest new year wish for you, {}!", "sent": False},
-                {"msg": "{}, your turn! :purple_heart: Make your 2020 a blast of fun, full of cheer and warm greetings for everyone! Happy New Year!", "sent": False},
-                {"msg": "Dear {}, I wish you happy New Year :green_heart:! I have a feeling 2020 is going to be a great year! Let's begin to plan new beginnings with renewed hope in the countless possibilities of the future!", "sent": False},
-                {"msg": "My lovely {}! :red_heart: Wishing you all the best for the coming year, with love and positive thoughts!", "sent": False},
-                ]
-
-
 family_chat = -1001186177604
 test_chat = -313765365
 
@@ -453,41 +389,6 @@ def send_family_chat_msg(bot, update):
     send_async(bot, chat_id=family_chat, text='{}'.format(update.message.text.split(" ", 1)[1]))
 
 
-def set_users(bot, update):
-    """ Unlocks the chat, so everyone can change settings """
-    chat_id = update.message.chat.id
-    db.set('user_timezones', USERS_AND_TIMEZONES)
-    db.set("congrats_msgs", congrats_mgs)
-
-    send_async(bot, chat_id=chat_id, text='Got it!')
-
-import pytz
-def timer(bot, update):
-    users = db.get("user_timezones")
-    congrats_ms = db.get("congrats_msgs")
-    con = ""
-    ready_users = []
-    for us in users:
-        tz = pytz.timezone(us["tz"])
-        need = datetime(2020, 1, 1, 0, 1, tzinfo=tz).strftime("%Y-%m-%d %H:%M")
-        current = datetime.now(tz).strftime("%Y-%m-%d %H:%M")
-        logger.info("need {} current {} user {}".format(need, current, us["name"]))
-        logger.info("need <= current {}".format(need <= current))
-        if need <= current:
-            logger.info("got it")
-            if us["congrats"] is False:
-                ready_users.append(us)
-                us["congrats"] = True
-    if len(ready_users) > 0:
-        db.set('user_timezones', users)
-        for msgs in congrats_ms:
-            if msgs["sent"] is False:
-                con = msgs["msg"]
-                msgs["sent"] = True
-                db.set("congrats_msgs", congrats_ms)
-                return congrats(bot, update, con, ready_users)
-
-
 def bis_bald(bot, update):
     chats = db.get('chats')
     if update.message.chat.id not in chats:
@@ -495,7 +396,6 @@ def bis_bald(bot, update):
         db.set('chats', chats)
         logger.info("I have been added to %d chats" % len(chats))
     logger.info("id: {}, name: {}".format(update.message.from_user.id, update.message.from_user.first_name))
-    timer(bot, update)
 
     if update.message.text is not None:
         msg = update.message.text.lower()
@@ -633,19 +533,6 @@ def send_sticker(bot, update, sticker, reply=False):
         bot.send_sticker(chat_id, sticker)
 
 
-def congrats(bot, update, msg, users):
-    message = update.message
-    chat_id = message.chat.id
-
-    # Replace placeholders and send message
-    dmslckm = []
-    for user in users:
-        dmslckm.append('<a href="tg://user?id={}">{}</a>'.format(user["id"], user["name"]))
-        #dmslckm.append('{}'.format( user["name"]))
-    text = emojize(msg.format(", ".join(dmslckm)))
-    send_async(bot, chat_id=chat_id, text=text, parse_mode=ParseMode.HTML)
-
-
 def error(bot, update, error, **kwargs):
     """ Error handling """
 
@@ -667,22 +554,6 @@ def error(bot, update, error, **kwargs):
         pass
 
 
-"""botan = None
-if BOTAN_TOKEN != 'BOTANTOKEN':
-    botan = Botan(BOTAN_TOKEN)
-
-@run_async
-def stats(bot, update, **kwargs):
-    if not botan:
-        return
-
-    if botan.track(update.message):
-        logger.debug("Tracking with botan.io successful")
-    else:
-        logger.info("Tracking with botan.io failed")
-        """
-
-
 def main():
     # Create the Updater and pass it your bot's token.
     #updater = Updater(TOKEN, workers=10, request_kwargs=REQUEST_KWARGS)
@@ -701,7 +572,6 @@ def main():
     dp.add_handler(CommandHandler("unlock", unlock))
     dp.add_handler(CommandHandler("quiet", quiet))
     dp.add_handler(CommandHandler("unquiet", unquiet))
-    dp.add_handler(CommandHandler("setusers", set_users))
     dp.add_handler(CommandHandler("sendtest", send_test_chat_msg))
     dp.add_handler(CommandHandler("sendfamily", send_family_chat_msg))
 
